2a3d32ede3ac73827f60bdc26d4d5bf789eac654
vicky2805vky <147974357+vicky2805vky@users.noreply.github.com>
2023-10-17 09:05:18 +0530

    Add files via upload

---
+++ challenge1.1.py
@@ -0,0 +1,11 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
+
+result = factorial(2)
+print(result)
---
+++ challenge3.1.py
@@ -0,0 +1,15 @@
+def  linearSearchProduct(productlist, targetproduct):
+  indices = []
+
+  for index, product in enumerate(productlist):
+    if product == targetproduct:
+      indices.append(index)
+
+  return indices 
+
+
+#Example usage:
+products = ["shoes","boot","loafer","shoes","sandal","shoes"]
+target = "shoes"
+result = linearSearchProduct(products, target)
+print(result)
